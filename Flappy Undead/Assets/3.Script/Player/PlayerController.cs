8fffce18de3eab14508506d3ffca829a7c9f1eb4
신성진 <ssin72787@gmail.com>
2024-06-28 15:54:01 +0900

    [Update] 스킬 스크립트 및 사운드 추가 작업

--- Flappy Undead/Assets/3.Script/Player/PlayerController.cs
+++ Flappy Undead/Assets/3.Script/Player/PlayerController.cs
@@ -6,14 +6,8 @@ public class PlayerController : MonoBehaviour
     public Player_Data data;
     public float Speed = 5f;
     public int Health = 2;
-    public float Cooltime = 5f;
-    public bool isDead = false;
-    public float LastSkillTime = 0f;
 
     public Rigidbody player_rid;
-    public Collider player_col;
-    public Renderer player_rend;
-    public Animator player_ani;
     public AudioSource player_au;
 
     public AudioClip jumpClip;
@@ -23,32 +17,16 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         player_rid = GetComponent<Rigidbody>();
-        player_col = GetComponent<Collider>();
-        player_rend = GetComponentInChildren<Renderer>();
-        player_ani = GetComponent<Animator>();
         player_au = GetComponent<AudioSource>();
 
         Speed = data.Speed;
         Health = data.Health;
-        Cooltime = data.CoolTime;
-        isDead = false;
-        LastSkillTime = 0f;
     }
     private void Update()
     {
-        if (isDead) return;
-
         if (Input.GetMouseButtonDown(0))
         {
             Jump();
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (Time.time >= LastSkillTime + Cooltime)
-            {
-                LastSkillTime = Time.time;
-                Use_Skill();
-            }
         }
     }
     public void Jump()
@@ -68,36 +46,6 @@ public class PlayerController : MonoBehaviour
     }
     public void Dead()
     {
-        isDead = true;
         player_au.PlayOneShot(deathClip);
-    }
-    public void Use_Skill()
-    {
-        switch (data.Type)
-        {
-            case Player_Data.CharType.Normal:
-                StartCoroutine(Invisible_co());
-                break;
-            case Player_Data.CharType.Witch:
-                StartCoroutine(Giant_co());
-                break;
-        }
-    }
-    public IEnumerator Invisible_co()
-    {
-        player_col.enabled = false;
-        player_rend.material.color = new Color(255/255f, 255/255f, 255/255f, 0.3f);
-        yield return new WaitForSeconds(3.0f);
-        player_col.enabled = true;
-        player_rend.material.color = new Color(255/255f, 255/255f, 255/255f, 1f);
-    }
-    public IEnumerator Giant_co()
-    {
-        player_ani.SetTrigger("Giant");
-        transform.tag = "Super";
-        yield return new WaitForSeconds(3.0f);
-        player_ani.SetTrigger("Small");
-        yield return new WaitForSeconds(1.0f);
-        transform.tag = "Player";
     }
 }
