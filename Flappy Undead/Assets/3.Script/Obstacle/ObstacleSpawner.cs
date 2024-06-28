0527c3f3956d425148bc74d2f5cf1ad26b3e4bcc
강유현 <soyuh1113@gmail.com>
2024-06-28 15:31:27 +0900

    [tEST] 장애물 생성기 테스트

--- Flappy Undead/Assets/3.Script/Obstacle/ObstacleSpawner.cs
+++ Flappy Undead/Assets/3.Script/Obstacle/ObstacleSpawner.cs
@@ -5,17 +5,61 @@ using UnityEngine;
 public class ObstacleSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject obstaclePrefab;
+    [SerializeField] private int initialObstacles = 7;
+    [SerializeField] private int poolCount = 15;
+    [SerializeField] private float spawnInterval = 2.0f;
+    private Queue<GameObject> poolQueue;
+    private float timer = 0;
 
-    [SerializeField] private float timeDiff;
-    [SerializeField] private float timer = 0;
+    private void Start()
+    {
+        InitializePool();
+        for (int i = 0; i < initialObstacles; i++)
+        {
+            SpawnObstacle();
+        }
+    }
 
     private void Update()
     {
         timer += Time.deltaTime;
-        if(timer > 1)
+        if (timer > spawnInterval)
         {
-            Instantiate(obstaclePrefab);
+            SpawnObstacle();
             timer = 0;
         }
+    }
+
+    private void InitializePool()
+    {
+        poolQueue = new Queue<GameObject>();
+
+        for (int i = 0; i < poolCount; i++)
+        {
+            GameObject obj = Instantiate(obstaclePrefab);
+            obj.SetActive(false);
+            poolQueue.Enqueue(obj);
+        }
+    }
+
+    private void SpawnObstacle()
+    {
+        if (poolQueue.Count > 0)
+        {
+            GameObject obstacle = poolQueue.Dequeue();
+            obstacle.transform.position = new Vector3(0, 0, -2.2f);
+            obstacle.SetActive(true);
+        }
+        else
+        {
+            GameObject obstacle = Instantiate(obstaclePrefab);
+            obstacle.transform.position = new Vector3(0, 0, -2.2f);
+        }
+    }
+
+    public void ReturnObstacle(GameObject obstacle)
+    {
+        obstacle.SetActive(false);
+        poolQueue.Enqueue(obstacle);
     }
 }
--- Flappy Undead/Assets/3.Script/Obstacle/Scroll_Obstacle.cs
+++ Flappy Undead/Assets/3.Script/Obstacle/Scroll_Obstacle.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Scroll_Obstacle : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] private float leftBound = -10.0f;
 
     private void Update()
     {
         transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+
+        if (transform.position.z < leftBound)
+        {
+            ObstacleSpawner spawner = FindObjectOfType<ObstacleSpawner>();
+            if (spawner != null)
+            {
+                spawner.ReturnObstacle(gameObject);
+            }
+        }
     }
 }
